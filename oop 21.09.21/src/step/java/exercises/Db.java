94fb5da8386168368bb3b15db6019f4cbc97e62b
Dmytro Podelnik <Pode_hr28@student.itstep.org>
2021-09-24 12:59:34 +0300

    update class

--- oop 21.09.21/src/step/java/exercises/Db.java
+++ oop 21.09.21/src/step/java/exercises/Db.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Db {
 
@@ -65,9 +66,41 @@ public class Db {
 
         // Queries
         // ! Single DB - user prefixes
+        // Update-Queries: DDL + DML (except SELECT)
+        String query;
+        /*
+                query = "CREATE TABLE " + PREFIX + "exercise( "
+                + "id RAW(16) DEFAULT SYS_GUID() PRIMARY KEY, "
+                + "name NVARCHAR2(64) NOT NULL,"
+                + "id_parent RAW(16) )";
+         */
+        /*
+        query = "INSERT INTO " + PREFIX + "exercise(name) " +
+                "VALUES('Petrovich')";
+         */
+        /*
+        query = "INSERT INTO " + PREFIX + "exercise(name, id_parent) " +
+                "VALUES('Lukich', (SELECT id FROM "
+                + PREFIX + "exercise WHERE name = 'Petrovich'))";
+         */
+        /*
+        query = String.format(
+                "INSERT INTO %sexercise(name, id_parent)" +
+                "VALUES('%s', (SELECT id FROM %sexercise WHERE name = '%s'))",
+                PREFIX, "Pavlovna", PREFIX, "Petrovich"
+        );
+         */
+        
+        try (Statement statement = connection.createStatement()) {
+            // statement ~ SqlCommand
+            statement.executeUpdate(query);
+        }
+        catch (SQLException ex) {
+            System.err.println(ex.getMessage() + " " + query);
+            return;
+        }
 
-
-        System.out.println(connectionString);
+        System.out.println("OK");
     }
 }
 /*
@@ -80,6 +113,13 @@ public class Db {
         ООП (Oracle)
     Стандарт SQL довольно старый (74-86). Все СУБД его реализуют, а также добавляют свои особенности.
         Это формирует диалекты SQL.
+        Кроме диалектов разные СУБД вводят свои типы данных
+        VARCHAR2(size) - 4k ANSI
+        NVARCHAR2(size)- 4k Localized
+        NUMBER(p, s) - p - цифр всего, s (scale) - точность (после .)
+        RAW(size) - бинарный (байтовый) тип
+        CLOB - Char LOB (~TEXT)
+        NCLOB
     СУБД (DBMS) играют роль сервера. Соответственно, требуют подключения.
         При подключении из "языка программирования" требуются специальные драйверы (коннекторы).
     После установки подключения языки программирования предлагают универсальный интерфейс для
