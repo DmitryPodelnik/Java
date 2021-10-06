96cb654e68fe56fc982f410d907b9aed172b0bcc
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-06 09:17:37 +0300

    "updated home"

---
+++ web1/src/main/java/step/java/web1/filters/DbFilter.java
@@ -0,0 +1,39 @@
+package step.java.web1.filters;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class DbFilter implements Filter {
+
+    private FilterConfig filterConfig;
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+        this.filterConfig = filterConfig;
+    }
+
+    @Override
+    public void doFilter(
+            ServletRequest servletRequest,
+            ServletResponse servletResponse,
+            FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        filterConfig = null;
+    }
+}
+
+/*
+    Сервлетные фильтры
+    Реализация концепции Middleware:
+    - Создание "цепочки" последовательных вызовов методов разных классов;
+    - Механизм автоматического встраивания новых классов в эту цепочку.
+    Для создания фильтра:
+    1. Реализуем интерфейс javax.servlet.Filter
+    2. Встраиваем фильтр:
+        а) через аннотацию @WebFilter
+        б) через web.xml
+ */
